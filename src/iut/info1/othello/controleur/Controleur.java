c74095b305bf7d0b8df4d81b9ecfe0ceb90205a7
Baudroit Leila <leila.baudroit@iut-rodez.fr>
2023-05-22 13:24:24 +0200

    Début du controleur

--- src/iut/info1/othello/controleur/Controleur.java
+++ src/iut/info1/othello/controleur/Controleur.java
@@ -1,7 +1,32 @@
 package iut.info1.othello.controleur;
 
-public class Controleur {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Controleur extends Application {
 	
 	
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		FXMLLoader chargeurFXML = new FXMLLoader();
+		chargeurFXML.setLocation(getClass().getResource("test/iut/info1/othello/vue/Jeu.fxml"));
+		Parent racine = chargeurFXML.load();
+		Scene scene = new Scene(racine);
+		
+		stage.setTitle("Othello");
+		stage.setScene(scene);
+		stage.show();
+		
+		
+	}
+
+	public static void main(String[] args) {
+		launch();
+		
+	}
 
 }
