b26f3b0033ab83a1cfbf9bf814cfb83e5d623a83
lbaudr <109075845+lbaudroit@users.noreply.github.com>
2023-05-26 09:34:57 +0200

    Changement config

--- src/iut/info1/othello/controleur/Controleur.java
+++ src/iut/info1/othello/controleur/Controleur.java
@@ -1,31 +1,9 @@
 package iut.info1.othello.controleur;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Controleur extends Application {
-	
-	
-	@Override
-	public void start(Stage stage) throws Exception {
-		
-		FXMLLoader chargeurFXML = new FXMLLoader();
-		chargeurFXML.setLocation(getClass().getResource("test/iut/info1/othello/vue/Jeu.fxml"));
-		Parent racine = chargeurFXML.load();
-		Scene scene = new Scene(racine);
-		
-		stage.setTitle("Othello");
-		stage.setScene(scene);
-		stage.show();
-		
-		
-	}
+public class Controleur {
 
 	public static void main(String[] args) {
-		launch();
+		System.out.println("Tamere");
 		
 	}
 
