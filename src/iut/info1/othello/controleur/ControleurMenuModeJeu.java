8c04afc5c4b7f187f0da9998c7a84f36b3d1daa7
lbaudr <109075845+lbaudroit@users.noreply.github.com>
2023-06-01 16:49:59 +0200

    Reliage des scènes de menu et ajout de javadoc

--- src/iut/info1/othello/controleur/ControleurMenuModeJeu.java
+++ src/iut/info1/othello/controleur/ControleurMenuModeJeu.java
@@ -10,27 +10,38 @@ import iut.info1.othello.modele.joueurs.JoueurHumain;
 import javafx.fxml.FXML;
 
 /**
- * Affiche le menu de choix du mode de jeu.
+ * Le contrôleur associé au menu du choix du mode de jeu.
+ * @author BAUDROIT Leïla
+ * @author BOYER Djedline
+ * @author BRIOT Nael
  */
 public class ControleurMenuModeJeu {
 	
-
-	public ControleurMenuModeJeu() {
-	}
-	
+	/**
+	 * Lance une partie entre joueurs humains et affiche l'écran de jeu.
+	 */
 	@FXML
 	public void creerPartiePVP() {
 		new Modele(new JoueurHumain(BLANC), new JoueurHumain(NOIR));
+		Main.changerScene(Main.SCENE_JEU);
 	}
 	
+	/**
+	 * Lance une partie avec une IA facile et affiche l'écran de jeu.
+	 */
 	@FXML
 	public void creerPartieIAFacile() {
 		new Modele(new JoueurHumain(BLANC), new IAFacile(NOIR));
+		Main.changerScene(Main.SCENE_JEU);
 	}
 	
+	/**
+	 * Lance une partie avec une IA difficile et affiche l'écran de jeu.
+	 */
 	@FXML
 	public void creerPartieIADifficile() {
 		new Modele(new JoueurHumain(BLANC), new IADifficile(NOIR));
+		Main.changerScene(Main.SCENE_JEU);
 	}
 
 }
