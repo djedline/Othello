bebf7f213066530cee1f313192c27c1ee9f98aa4
djedline <103513050+djedline@users.noreply.github.com>
2023-06-02 10:45:54 +0200

    Voilà

--- src/iut/info1/othello/vue/ControleurJeu.java
+++ src/iut/info1/othello/vue/ControleurJeu.java
@@ -1,44 +1,22 @@
 package iut.info1.othello.vue;
 
-import javafx.application.Application;
+
+
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 
 /** TODO comment class responsibility (SRP)
  * @author Djed
  *
  */
-public class ControleurJeu extends Application {
+public class ControleurJeu {
 
     /** créer un gridpane */
     @FXML
-    public static GridPane pane;
+    private GridPane pane;
 
-    @Override
-    public void start(Stage stage) throws Exception {
-
-        FXMLLoader chargeurFXML = new FXMLLoader();
-        chargeurFXML.setLocation(getClass().getResource("Jeu.fxml"));
-        Parent racine = chargeurFXML.load();
-        Scene scene = new Scene(racine);
-        //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-
-        stage.setTitle("Othello");
-        stage.setScene(scene);
-        stage.show();
-
-       // System.out.println(pane.toString());
-    }
-
-
-    /** TODO comment method role
-     * @param args
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
+    @FXML
+    private MenuItem Menu;
+    
 }
